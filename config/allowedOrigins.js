5d374581c6f8c122aecd6eb702fb5d9a43fa98f8
Ahmad Saulawa <ahmadlawal020@gmail.com>
2025-08-27 07:58:14 +0100

    final commit

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins = [
   "http://localhost:8080",
-  "https://learningmanagementt.netlify.app/",
   "https://learningmanagementt.netlify.app",
 ];
 
--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,11 +1,27 @@
+// const allowedOrigins = require("./allowedOrigins");
+
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+//   optionsSuccessStatus: 200,
+// };
+
+// module.exports = corsOptions;
+
 const allowedOrigins = require("./allowedOrigins");
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error(`CORS blocked: ${origin}`));
     }
   },
   credentials: true,
